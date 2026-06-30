33ae3e01031cd57ee31e49334a909152115e70ea
richmtx <martinezhernandezricardo06@gmail.com>
2026-06-30 14:58:53 -0600

    Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,28 +11,28 @@ export const routes: Routes = [
   {
     path: 'programacion',
     loadComponent: () =>
-      import('./features/home/pages/programacion/programacion.component').then(
+      import('./features/programacion/programacion.component').then(
         (m) => m.ProgramacionComponent
       ),
   },
   {
     path: 'acerca',
     loadComponent: () =>
-      import('./features/home/pages/acerca/acerca.component').then(
+      import('./features/acerca/acerca.component').then(
         (m) => m.AcercaComponent
       ),
   },
   {
     path: 'videoteca',
     loadComponent: () =>
-      import('./features/home/pages/videoteca/videoteca.component').then(
+      import('./features/videoteca/videoteca.component').then(
         (m) => m.VideotecaComponent
       ),
   },
   {
   path: 'en-vivo',
   loadComponent: () =>
-    import('./features/home/pages/en-vivo/en-vivo.component').then(
+    import('./features/en-vivo/en-vivo.component').then(
       (m) => m.EnVivoComponent
     ),
 },
