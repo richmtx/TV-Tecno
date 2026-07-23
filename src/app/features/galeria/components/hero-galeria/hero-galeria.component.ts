ef71d27ff7b6ad9b4c4327d8c638a2e8c01bb5e4
richmtx <martinezhernandezricardo06@gmail.com>
2026-07-23 09:50:15 -0600

    Commit

--- src/app/features/galeria/components/hero-galeria/hero-galeria.component.ts
+++ src/app/features/galeria/components/hero-galeria/hero-galeria.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-hero-galeria',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './hero-galeria.component.html',
   styleUrl: './hero-galeria.component.css'
 })
