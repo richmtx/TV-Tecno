28ee066947d1cfe398af9f6ee8022f0601d2b837
richmtx <martinezhernandezricardo06@gmail.com>
2026-07-25 17:24:26 -0600

    Commit

--- src/app/features/programacion/components/en-vivo-ahora/en-vivo-ahora.component.ts
+++ src/app/features/programacion/components/en-vivo-ahora/en-vivo-ahora.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Programa, ICONOS_CATEGORIA } from '../../models/programa.model';
 
 @Component({
     selector: 'app-en-vivo-ahora',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './en-vivo-ahora.component.html',
     styleUrl: './en-vivo-ahora.component.css'
 })
