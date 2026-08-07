97eb5a3c118eada335eaf35201e219a339522808
richmtx <martinezhernandezricardo06@gmail.com>
2026-08-06 21:42:42 -0600

    Commit

--- src/app/features/videoteca/pages/videoteca/videoteca.component.ts
+++ src/app/features/videoteca/pages/videoteca/videoteca.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { VideotecaHeroComponent } from '../../components/videoteca-hero/videoteca-hero.component';
 import { VideotecaCatalogoComponent } from "../../components/videoteca-catalogo/videoteca-catalogo.component";
+import { YoutubeComponent } from "../../../../shared/components/youtube/youtube.component";
 
 @Component({
   selector: 'app-videoteca',
-  imports: [VideotecaHeroComponent, VideotecaCatalogoComponent],
+  imports: [VideotecaHeroComponent, VideotecaCatalogoComponent, YoutubeComponent],
   templateUrl: './videoteca.component.html',
   styleUrl: './videoteca.component.css'
 })
