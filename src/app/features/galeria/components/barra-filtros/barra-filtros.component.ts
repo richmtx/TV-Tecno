7ae1d99575e3658c22648fb4be5292cc683092a3
richmtx <martinezhernandezricardo06@gmail.com>
2026-08-07 20:18:17 -0600

    Commit

---
+++ src/app/features/galeria/components/barra-filtros/barra-filtros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-filtros',
+  imports: [],
+  templateUrl: './barra-filtros.component.html',
+  styleUrl: './barra-filtros.component.css'
+})
+export class BarraFiltrosComponent {
+
+}
---
+++ src/app/features/galeria/components/galeria-tabs/galeria-tabs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria-tabs',
+  imports: [],
+  templateUrl: './galeria-tabs.component.html',
+  styleUrl: './galeria-tabs.component.css'
+})
+export class GaleriaTabsComponent {
+
+}
