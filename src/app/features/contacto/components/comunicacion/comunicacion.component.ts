f01804d95e1a9bd98946a8c9779414a5250c1a3c
richmtx <martinezhernandezricardo06@gmail.com>
2026-08-11 13:57:33 -0600

    Commit

---
+++ src/app/features/contacto/components/comunicacion/comunicacion.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comunicacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './comunicacion.component.html',
+  styleUrl: './comunicacion.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ComunicacionComponent { }
--- src/app/features/contacto/pages/contacto/contacto.component.ts
+++ src/app/features/contacto/pages/contacto/contacto.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { ComunicacionComponent } from '../../components/comunicacion/comunicacion.component';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
+  imports: [HeroComponent, ComunicacionComponent],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrl: './contacto.component.css',
 })
-export class ContactoComponent {
-
-}
+export class ContactoComponent {}
